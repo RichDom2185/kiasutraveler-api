0fc2603dd53efff25330f38f0ce3d23f158d0b73
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-03-29 22:49:03 +0800

    Remove debug statements

--- src/index.js
+++ src/index.js
@@ -18,9 +18,4 @@ app.use("/ride_hailing", rideHailingRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
-  const body = {
-    pickUp: { latitude: 1.3557467791, longitude: 103.9871103 },
-    dropOff: { latitude: 1.2943507656757305, longitude: 103.77389728148182 },
-  };
-  console.log(JSON.stringify(body).length);
 });
