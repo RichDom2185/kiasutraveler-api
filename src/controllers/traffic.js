a18bb76c6539973e8fd0092d3c2449030a0352d0
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-03-31 21:33:31 +0800

    Add bus density endpoint

--- src/controllers/traffic.js
+++ src/controllers/traffic.js
@@ -1,15 +1,14 @@
+import AdmZip from "adm-zip";
 import axios from "axios";
 
 const LTA_DATAMALL_TRAFFIC_INCIDENTS_ENDPOINT =
   "http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents";
 
 export const getTrafficIncidents = async token => {
-  const options = {
-    headers: {
-      AccountKey: token,
-    },
-  };
-  const res = await axios.get(LTA_DATAMALL_TRAFFIC_INCIDENTS_ENDPOINT, options);
+  const headers = { AccountKey: token };
+  const res = await axios.get(LTA_DATAMALL_TRAFFIC_INCIDENTS_ENDPOINT, {
+    headers,
+  });
 
   const incidents = res.data["value"];
 
@@ -23,5 +22,34 @@ export const getTrafficIncidents = async token => {
         details: Message,
       };
     }),
+  };
+};
+
+const LTA_DATAMALL_TRAFFIC_DENSITY_BUS_ENDPOINT =
+  "http://datamall2.mytransport.sg/ltaodataservice/PV/Bus";
+
+export const getTrafficDensityBus = async (token, date) => {
+  const headers = { AccountKey: token };
+  const res = await axios.get(LTA_DATAMALL_TRAFFIC_DENSITY_BUS_ENDPOINT, {
+    headers,
+  });
+
+  // Links for zip files containing the data
+  const links = res.data["value"].map(({ Link }) => Link);
+  const csvs = [];
+  await Promise.all(
+    links.map(async link => {
+      const zipResponse = await axios.get(link, {
+        responseType: "arraybuffer",
+      });
+
+      const zip = new AdmZip(zipResponse.data);
+      const files = zip.getEntries();
+      files.forEach(file => csvs.push(zip.readAsText(file)));
+    })
+  );
+
+  return {
+    data: csvs,
   };
 };
--- src/routes/traffic.js
+++ src/routes/traffic.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { LTA_DATAMALL_API_KEY } from "../constants.js";
-import { getTrafficIncidents } from "../controllers/traffic.js";
+import {
+  getTrafficDensityBus,
+  getTrafficIncidents,
+} from "../controllers/traffic.js";
 
 const router = express.Router();
 
 router.get("/incidents", (req, res) => {
   getTrafficIncidents(LTA_DATAMALL_API_KEY).then(data => res.json(data));
+});
+
+router.get("/density", (req, res) => {
+  getTrafficDensityBus(LTA_DATAMALL_API_KEY).then(data => res.json(data));
 });
 
 export default router;
