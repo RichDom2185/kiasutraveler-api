28ab0bbf2ccd0eef1ecbc5fa982fe05edc8f90ff
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-03-31 20:57:05 +0800

    Add LTA DataMall API key

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 export const GRAB_ENDPOINT_URL =
   "https://www.grab.com/wp-json/api/farefeed/v1/estimate";
 export const USER_AGENT =
@@ -5,3 +9,5 @@ export const USER_AGENT =
 
 export const ONEMAP_API_GET_TOKEN_ENDPOINT =
   "https://developers.onemap.sg/privateapi/auth/post/getToken";
+
+export const LTA_DATAMALL_API_KEY = process.env.LTA_DATAMALL_API_KEY;
