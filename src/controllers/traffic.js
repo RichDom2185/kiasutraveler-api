590470f93c19605bc320d7b3b62c8ed6d9165c1a
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-03-31 22:11:12 +0800

    Add real-time MRT platform density endpoint

--- src/controllers/traffic.js
+++ src/controllers/traffic.js
@@ -53,3 +53,83 @@ export const getTrafficDensityBus = async (token, date) => {
     data: csvs,
   };
 };
+
+const LTA_DATAMALL_TRAFFIC_DENSITY_PLATFORM_ENDPOINT =
+  "http://datamall2.mytransport.sg/ltaodataservice/PCDRealTime";
+
+// From https://datamall.lta.gov.sg/content/dam/datamall/datasets/LTA_DataMall_API_User_Guide.pdf
+export const MRT_LINE_ALL = "all"; // Special value
+export const MRT_LINE_CCL = "CCL";
+export const MRT_LINE_CEL = "CEL";
+export const MRT_LINE_CGL = "CGL";
+export const MRT_LINE_DTL = "DTL";
+export const MRT_LINE_EWL = "EWL";
+export const MRT_LINE_NEL = "NEL";
+export const MRT_LINE_NSL = "NSL";
+export const MRT_LINE_BPL = "BPL";
+export const MRT_LINE_SLRT = "SLRT";
+export const MRT_LINE_PLRT = "PLRT";
+export const MRT_LINES = [
+  // do not include MRT_LINE_ALL
+  MRT_LINE_CCL,
+  MRT_LINE_CEL,
+  MRT_LINE_CGL,
+  MRT_LINE_DTL,
+  MRT_LINE_EWL,
+  MRT_LINE_NEL,
+  MRT_LINE_NSL,
+  MRT_LINE_BPL,
+  MRT_LINE_SLRT,
+  MRT_LINE_PLRT,
+];
+
+// From https://datamall.lta.gov.sg/content/dam/datamall/datasets/LTA_DataMall_API_User_Guide.pdf
+const CROWD_LEVEL_LOW = "l";
+const CROWD_LEVEL_MODERATE = "m"; // Not used
+const CROWD_LEVEL_HIGH = "h"; // Not used
+const CROWD_LEVEL_NA = "na"; // Not used
+
+export const getTrafficDensityPlatform = async (token, mrtLine) => {
+  const headers = { AccountKey: token };
+  const params = {
+    TrainLine: mrtLine,
+  };
+  const res = await axios.get(LTA_DATAMALL_TRAFFIC_DENSITY_PLATFORM_ENDPOINT, {
+    headers,
+    params,
+  });
+
+  const crowds = res.data["value"];
+
+  return {
+    // List of all stations queried
+    stations: crowds.map(({ Station }) => Station),
+    // List of stations with crowd alerts
+    crowds: crowds
+      // Only returns stations with non-low crowding to save bandwidth
+      .filter(({ CrowdLevel }) => CrowdLevel !== CROWD_LEVEL_LOW)
+      .map(({ Station, CrowdLevel }) => {
+        return {
+          code: Station,
+          crowdLevel: CrowdLevel,
+        };
+      }),
+  };
+};
+
+export const getTrafficDensityAllPlatforms = async token => {
+  const allStations = [];
+  const allCrowds = [];
+  for (const mrtLine of MRT_LINES) {
+    const { stations, crowds } = await getTrafficDensityPlatform(
+      token,
+      mrtLine
+    );
+    allStations.push(...stations);
+    allCrowds.push(...crowds);
+  }
+  return {
+    stations: allStations,
+    crowds: allCrowds,
+  };
+};
--- src/routes/traffic.js
+++ src/routes/traffic.js
@@ -1,7 +1,20 @@
 import express from "express";
 import { LTA_DATAMALL_API_KEY } from "../constants.js";
 import {
+  MRT_LINE_ALL,
+  MRT_LINE_BPL,
+  MRT_LINE_CCL,
+  MRT_LINE_CEL,
+  MRT_LINE_CGL,
+  MRT_LINE_DTL,
+  MRT_LINE_EWL,
+  MRT_LINE_NEL,
+  MRT_LINE_NSL,
+  MRT_LINE_PLRT,
+  MRT_LINE_SLRT,
+  getTrafficDensityAllPlatforms,
   getTrafficDensityBus,
+  getTrafficDensityPlatform,
   getTrafficIncidents,
 } from "../controllers/traffic.js";
 import { errorBadRequest, errorUnprocessableEntity } from "../utils/errors.js";
@@ -22,6 +35,37 @@ router.get("/density", (req, res) => {
     case "bus":
       getTrafficDensityBus(LTA_DATAMALL_API_KEY).then(data => res.json(data));
       return;
+    case "platform":
+      const { mrt } = req.query;
+      if (!mrt) {
+        errorBadRequest(res);
+        return;
+      }
+      switch (mrt) {
+        case MRT_LINE_CCL:
+        case MRT_LINE_CEL:
+        case MRT_LINE_CGL:
+        case MRT_LINE_DTL:
+        case MRT_LINE_EWL:
+        case MRT_LINE_NEL:
+        case MRT_LINE_NSL:
+        case MRT_LINE_BPL:
+        case MRT_LINE_SLRT:
+        case MRT_LINE_PLRT:
+          getTrafficDensityPlatform(LTA_DATAMALL_API_KEY, mrt).then(data =>
+            res.json(data)
+          );
+          return;
+        case MRT_LINE_ALL:
+          getTrafficDensityAllPlatforms(LTA_DATAMALL_API_KEY).then(data =>
+            res.json(data)
+          );
+          return;
+        default:
+          errorUnprocessableEntity(res);
+          return;
+      }
+
     default:
       errorUnprocessableEntity(res);
       return;
