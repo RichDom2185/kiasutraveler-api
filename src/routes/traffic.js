3d0297f218aad7ecb97c06b7e6cce27e1061d515
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-03-31 21:39:29 +0800

    Add guard clauses

--- src/routes/traffic.js
+++ src/routes/traffic.js
@@ -4,6 +4,7 @@ import {
   getTrafficDensityBus,
   getTrafficIncidents,
 } from "../controllers/traffic.js";
+import { errorBadRequest, errorUnprocessableEntity } from "../utils/errors.js";
 
 const router = express.Router();
 
@@ -12,7 +13,19 @@ router.get("/incidents", (req, res) => {
 });
 
 router.get("/density", (req, res) => {
-  getTrafficDensityBus(LTA_DATAMALL_API_KEY).then(data => res.json(data));
+  const { type } = req.query;
+  if (!type) {
+    errorBadRequest(res);
+    return;
+  }
+  switch (type) {
+    case "bus":
+      getTrafficDensityBus(LTA_DATAMALL_API_KEY).then(data => res.json(data));
+      return;
+    default:
+      errorUnprocessableEntity(res);
+      return;
+  }
 });
 
 export default router;
