435ebbf3df3c711a819c194086ce38504017a6b1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-03-31 20:50:28 +0800

    Create controllers for geocode endpoints

---
+++ src/controllers/geocode.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+const ONEMAP_API_SEARCH_ENDPOINT =
+  "https://developers.onemap.sg/commonapi/search";
+
+// https://www.onemap.gov.sg/docs/#search
+export const getCoordinatesFromAddress = async (
+  address,
+  maxNumOfResults = 8
+) => {
+  const params = {
+    searchVal: address,
+    returnGeom: "Y",
+    getAddrDetails: "Y",
+  };
+
+  const res = await axios.get(ONEMAP_API_SEARCH_ENDPOINT, { params });
+  const results = res.data["results"];
+  return {
+    results: results
+      .slice(0, maxNumOfResults)
+      .map(({ SEARCHVAL, BLK_NO, ROAD_NAME, POSTAL, LATITUDE, LONGITUDE }) => {
+        return {
+          name: SEARCHVAL,
+          block: BLK_NO,
+          roadName: ROAD_NAME,
+          postalCode: POSTAL,
+          lat: LATITUDE,
+          lng: LONGITUDE,
+        };
+      }),
+  };
+};
+
+const ONEMAP_API_REVERSE_GEOCODE_ENDPOINT =
+  "https://developers.onemap.sg/privateapi/commonsvc/revgeocode";
+
+// https://www.onemap.gov.sg/docs/#reverse-geocode-wgs84
+export const getAddressesFromCoordinates = async (
+  token,
+  latitude,
+  longitude,
+  maxDistance = 10 // In metres. Must be between [0, 500]
+) => {
+  const params = {
+    location: `${latitude},${longitude}`,
+    token,
+    buffer: maxDistance,
+    otherFeatures: "Y",
+  };
+
+  const res = await axios.get(ONEMAP_API_REVERSE_GEOCODE_ENDPOINT, { params });
+  const buildings = res.data["GeocodeInfo"];
+
+  return {
+    buildings: buildings.map(
+      ({ BUILDINGNAME, BLOCK, ROAD, POSTALCODE, LATITUDE, LONGITUDE }) => {
+        return {
+          name: BUILDINGNAME,
+          block: BLOCK,
+          roadName: ROAD,
+          postalCode: POSTALCODE,
+          lat: parseFloat(LATITUDE),
+          lng: parseFloat(LONGITUDE),
+        };
+      }
+    ),
+  };
+};
--- src/routes/geocode.js
+++ src/routes/geocode.js
@@ -1,13 +1,43 @@
 import express from "express";
+import {
+  getAddressesFromCoordinates,
+  getCoordinatesFromAddress,
+} from "../controllers/geocode.js";
+import { errorBadRequest, errorUnprocessableEntity } from "../utils/errors.js";
+import { getOneMapApiToken } from "../utils/functions.js";
 
 const router = express.Router();
 
 router.get("/forward", (req, res) => {
-  res.send("Forward");
+  const { address, maxCount } = req.query;
+  if (!address) {
+    errorBadRequest(res);
+    return;
+  }
+  const args = [address];
+  if (maxCount) {
+    args.push(maxCount);
+  }
+  getCoordinatesFromAddress(...args).then(data => res.json(data));
 });
 
 router.get("/reverse", (req, res) => {
-  res.send("Reverse");
+  getOneMapApiToken().then(token => {
+    let { lat, lng } = req.query;
+    if (!lat || !lng) {
+      errorBadRequest(res);
+      return;
+    }
+    try {
+      lat = parseFloat(lat);
+      lng = parseFloat(lng);
+    } catch (e) {
+      errorUnprocessableEntity(res);
+      return;
+    }
+    // TODO: Use `maxDistance` param
+    getAddressesFromCoordinates(token, lat, lng).then(data => res.json(data));
+  });
 });
 
 export default router;
