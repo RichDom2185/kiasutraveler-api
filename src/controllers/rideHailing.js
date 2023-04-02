10fc95e41056bd9e2bbf3fe8cbfa726485df2e82
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-02 14:15:53 +0800

    Add taxi availability endpoint

--- src/controllers/rideHailing.js
+++ src/controllers/rideHailing.js
@@ -3,7 +3,11 @@ import axios from "axios";
 import { USER_AGENT } from "../constants.js";
 
 export const RIDE_HAILING_TYPE_GRAB = "grab";
-export const RIDE_HAILING_SERVICES = [RIDE_HAILING_TYPE_GRAB];
+export const RIDE_HAILING_TYPE_TAXI = "taxi";
+export const RIDE_HAILING_SERVICES = [
+  RIDE_HAILING_TYPE_GRAB,
+  RIDE_HAILING_TYPE_TAXI,
+];
 
 const GRAB_AVAILABLE_SERVICES_ENDPOINT =
   "https://www.grab.com/wp-json/api/farefeed/v1/estimate";
@@ -45,4 +49,13 @@ export const getAvailableGrabServices = async (
   );
 
   return { services: parsedServices };
+};
+
+const DATA_GOV_TAXI_AVAILABILITY_ENDPOINT =
+  "https://api.data.gov.sg/v1/transport/taxi-availability";
+
+// No manipulation needed, data is already in GeoJSON format
+export const getAvailableTaxis = async () => {
+  const res = await axios.get(DATA_GOV_TAXI_AVAILABILITY_ENDPOINT);
+  return res.data;
 };
--- src/routes/rideHailing.js
+++ src/routes/rideHailing.js
@@ -2,36 +2,47 @@ import express from "express";
 
 import {
   getAvailableGrabServices,
-  RIDE_HAILING_SERVICES,
+  getAvailableTaxis,
+  RIDE_HAILING_TYPE_GRAB,
+  RIDE_HAILING_TYPE_TAXI,
 } from "../controllers/rideHailing.js";
 import { errorBadRequest, errorUnprocessableEntity } from "../utils/errors.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  let { startLat, startLng, endLat, endLng } = req.query;
   const { serviceType } = req.query;
-  if (!startLat || !startLng || !endLat || !endLng || !serviceType) {
+  if (!serviceType) {
     errorBadRequest(res);
     return;
   }
-  try {
-    startLat = parseFloat(startLat);
-    startLng = parseFloat(startLng);
-    endLat = parseFloat(endLat);
-    endLng = parseFloat(endLng);
-  } catch (e) {
-    errorUnprocessableEntity(res);
-    return;
+  switch (serviceType) {
+    case RIDE_HAILING_TYPE_GRAB:
+      let { startLat, startLng, endLat, endLng } = req.query;
+      if (!startLat || !startLng || !endLat || !endLng) {
+        errorBadRequest(res);
+        return;
+      }
+      try {
+        startLat = parseFloat(startLat);
+        startLng = parseFloat(startLng);
+        endLat = parseFloat(endLat);
+        endLng = parseFloat(endLng);
+      } catch (e) {
+        errorUnprocessableEntity(res);
+        return;
+      }
+      getAvailableGrabServices(startLat, startLng, endLat, endLng).then(data =>
+        res.json(data)
+      );
+      return;
+    case RIDE_HAILING_TYPE_TAXI:
+      getAvailableTaxis().then(data => res.json(data));
+      return;
+    default:
+      errorUnprocessableEntity(res);
+      return;
   }
-  if (!RIDE_HAILING_SERVICES.includes(serviceType)) {
-    errorUnprocessableEntity(res);
-    return;
-  }
-
-  getAvailableGrabServices(startLat, startLng, endLat, endLng).then(data =>
-    res.json(data)
-  );
 });
 
 export default router;
