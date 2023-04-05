a27a8014cd62ed226ff17d4e28e19a997f0394c1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-04-06 02:26:09 +0800

    Add weather forecast endpoint

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -1,5 +1,35 @@
 import axios from "axios";
 
+const DATA_GOV_FORECAST_ENDPOINT =
+  "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast";
+
+export const getWeatherForecast = async (date, time) => {
+  const params = {
+    date: date && !time ? date : undefined,
+    date_time: date && time ? `${date}T${time}` : undefined,
+  };
+  const res = await axios.get(DATA_GOV_FORECAST_ENDPOINT, { params });
+  const { area_metadata, items } = res.data;
+  return {
+    areas: area_metadata.map(
+      ({ name, label_location: { latitude, longitude } }) => {
+        return {
+          name,
+          lat: latitude,
+          lng: longitude,
+        };
+      }
+    ),
+    data: items.map(({ valid_period: { start, end }, forecasts }) => {
+      return {
+        startTime: start,
+        endTime: end,
+        forecasts,
+      };
+    }),
+  };
+};
+
 const DATA_GOV_RAINFALL_ENDPOINT =
   "https://api.data.gov.sg/v1/environment/rainfall";
 
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,8 +1,16 @@
 import express from "express";
 
-import { getRainfallData } from "../controllers/weather.js";
+import { getRainfallData, getWeatherForecast } from "../controllers/weather.js";
 
 const router = express.Router();
+
+router.get("/forecast", (req, res) => {
+  const { date, time } = req.query;
+  // TODO: Validate date and time are YYYY-MM-DD and HH:mm:ss
+  //       respectively, if they are provided.
+  // Undefined is fine, as that will just return the current data
+  getWeatherForecast(date, time).then(data => res.json(data));
+});
 
 router.get("/rainfall", (req, res) => {
   const { date, time } = req.query;
